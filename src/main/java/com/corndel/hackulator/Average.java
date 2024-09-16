1d1de54db552db0a32fc4337c1f93a5e679eec3a
Simon Haines <nimon@hotmail.co.uk>
2024-09-16 06:24:26 +0100

    solves average

--- src/main/java/com/corndel/hackulator/Average.java
+++ src/main/java/com/corndel/hackulator/Average.java
@@ -4,9 +4,10 @@ public class Average {
   /**
    * Divides the numbers
    *
-   * <p>divide(15, 3) => 5
+   * <p>
+   * divide(15, 3) => 5
    */
   static int average(int x, int y) {
-    return x + y / 2;
+    return (x + y) / 2;
   }
 }
